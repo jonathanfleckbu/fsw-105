e8503a3a84ca1c1b38a5c67ec5190acfcea6999e
jonathanfleckbu <jonathan.fleck@bryanuniversity.edu>
2020-03-08 11:25:52 -0700

    commit of jscrpt calc

---
+++ week4/Javascript_calculator/jscript_calculator_in_node.js
@@ -0,0 +1,41 @@
+const readlineSync = require("readline-sync")
+
+function addTwoNumbers(num1, num2){
+    let sum = num1 + num2;
+    console.log("The result: ", sum)
+}
+
+function timesTwoNum(num1, num2){
+    let multipliedSum = num1 * num2;
+    console.log("The result: ", multipliedSum)
+}
+
+function dividesTwo(num1, num2){
+    let devidedSum = num1 / num2;
+    console.log("The result: ", devidedSum)
+}
+
+function subtractsTwo(num1, num2){
+    let subtractedSum = num1 - num2;
+    console.log("The result: ", subtractedSum)
+}
+
+
+var num1 = readlineSync.questionInt("What is your first number? ");
+console.log('This number ' + num1 + '!');
+
+var num2 = readlineSync.questionInt("What is your second number? ");
+console.log('This number ' + num2 + '!');
+
+var whatDoYouWant = readlineSync.question("What would you like to do with the numbers? / add, sub, mult, or div? ")
+console.log(whatDoYouWant)
+
+if(whatDoYouWant === "add"){
+    addTwoNumbers(num1, num2)
+}else if(whatDoYouWant === "mult"){
+    timesTwoNum(num1, num2)
+}else if(whatDoYouWant === "div"){
+    dividesTwo(num1, num2)
+}else{
+    subtractsTwo(num1, num2)
+}
